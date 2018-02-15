1a8d2b9e48da21d74097089b9871129d0b101453
chandu2chandu1 <chandu2chandu1@gmail.com>
2018-02-15 15:25:45 +0530

    Added the basic layout for the School Management

--- web-client/src/app/app.module.ts
+++ web-client/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { MainMenuComponent } from './main-menu/main-menu.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainMenuComponent
   ],
   imports: [
     BrowserModule
